a6808a88a8324dd3b1d478c0d737de2dd23c8a3a
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-02-09 18:07:52 -0300

    parte 3

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-console.log(consultaCep)
+const consultaCep = fetch('https://viacep.com.br/ws/06395280/json/')
+    .then(resposta => resposta.json())
+    .then(r => {
+        if(r.erro){
+            throw Error('Esse cep não existe!')
+        }
+    console.log(r.cep)
+    console.log(r.logradouro)
+    console.log(r.bairro)
+    console.log(r.uf)
+    console.log(r)
+    })
+    .catch(erro => console.log(erro))
+    .finally(mensagem => console.log('Processamento conscluído!'))
