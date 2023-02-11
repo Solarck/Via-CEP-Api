092c654fc753be89d60888c7c64e06ef573ed0d6
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-02-11 19:29:55 -0300

    aula 5 continuação

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        const consultaCep = await fetch('https://viacep.com.br/ws/06395280/json/')
+        const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         let consultaCepConvertida = await consultaCep.json()
 
         if (consultaCepConvertida.erro) {
             throw Error('Cep não existente!')
         }
 
+        const cidade = document.getElementById('cidade')
+        const logradouro = document.getElementById('endereco')
+        const estado = document.getElementById('estado')
+        const bairro = document.getElementById('bairro')
+
+        cidade.value = consultaCepConvertida.localidade
+        logradouro.value = consultaCepConvertida.logradouro
+        estado.value = consultaCepConvertida.uf
+        bairro.value = consultaCepConvertida.bairro
+
         console.log(consultaCepConvertida)
+        return consultaCepConvertida
 
     } catch (erro) {
         console.log(erro)
     }
 }
 
-buscaEndereco()
+const cep = document.getElementById('cep')
+// focusout é um evento que funciona após clicar fora do campo selecionado.
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
 
